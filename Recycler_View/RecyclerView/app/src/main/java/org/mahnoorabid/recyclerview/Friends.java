7d79a8ee47d6a004c2abed39207499a33d9ce2ff
MahnoorAbid <mcsf19a030@pucit.edu.pk>
2021-04-13 10:46:08 +0500

    first commit

---
+++ Recycler_View/RecyclerView/app/src/main/java/org/mahnoorabid/recyclerview/Friends.java
@@ -0,0 +1,6 @@
+package org.mahnoorabid.recyclerview;
+
+public class Friends {
+    public Friends(int i, String asad, int i1, String giglgit) {
+    }
+}
---
+++ Recycler_View/RecyclerView/app/src/main/java/org/mahnoorabid/recyclerview/RecyclerViewAdapter.java
@@ -0,0 +1,10 @@
+package org.mahnoorabid.recyclerview;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public abstract class RecyclerViewAdapter extends RecyclerView.Adapter {
+    public RecyclerViewAdapter(List<Friends> friendsList, MainActivity mainActivity) {
+    }
+}
